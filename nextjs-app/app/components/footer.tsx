0c577171efa5cc4b96c1275a6d1cfb0c91bd7979
Adam Danielsson <adam.ov.danielsson@gmail.com>
2024-11-27 11:26:03 +0100

    debug: module not found error

---
+++ nextjs-app/app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-50 border-gray-100 border-t">
+      a site built by a nice person!
+    </footer>
+  );
+}
--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "placehold.co" },
     ],
   },
+   webpack: (config) => {
+    config.resolve.caseSensitive = true;
+    return config;
+  }
 };
 
 export default nextConfig;
