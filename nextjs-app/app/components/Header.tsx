a2b48a73ccc798450e1ae85127838bc397132d5e
Adam Danielsson <99751404+adamovd@users.noreply.github.com>
2024-11-28 12:11:17 +0100

    Delete nextjs-app/app/components/Header.tsx

--- nextjs-app/app/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Header() {
-  return (
-    <header className="fixed z-50 h-24 inset-0 bg-white/80 flex items-center backdrop-blur-lg">
-      <h1>Test</h1>
-    </header>
-  );
-}
