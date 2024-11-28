aff2235bc61b9ea5f91c9f8cee54b775ffe9f636
Adam Danielsson <adam.ov.danielsson@gmail.com>
2024-11-28 09:46:22 +0100

    debug: error on deploy

--- nextjs-app/app/events/[slug]/page.tsx
+++ nextjs-app/app/events/[slug]/page.tsx
@@ -1,11 +1,13 @@
-import { client } from "@/sanity/lib/client";
-import { sanityFetch } from "@/sanity/lib/live";
-import imageUrlBuilder from "@sanity/image-url";
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
-import { defineQuery } from "next-sanity";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+
+import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { defineQuery } from "next-sanity";
+
+import { client } from "@/sanity/lib/client";
+import { sanityFetch } from "@/sanity/lib/live";
 
 const EVENT_QUERY = defineQuery(`*[
     _type == "event" &&
@@ -46,7 +48,7 @@ export default async function EventPage({
       <div className="mb-4">
         <Link href="/">← Back to events</Link>
       </div>
-      <div className="grid items-top gap-12 sm:grid-cols-2">
+      <div className="items-top grid gap-12 sm:grid-cols-2">
         <Image
           src={eventImageUrl || "https://placehold.co/550x310/png"}
           alt={name || "Event"}
@@ -57,18 +59,18 @@ export default async function EventPage({
         <div className="flex flex-col justify-center space-y-4">
           <div className="space-y-4">
             {eventType ? (
-              <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800 capitalize">
+              <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm capitalize dark:bg-gray-800">
                 {eventType.replace("-", " ")}
               </div>
             ) : null}
             {name ? (
-              <h1 className="text-4xl font-bold tracking-tighter mb-8">
+              <h1 className="mb-8 text-4xl font-bold tracking-tighter">
                 {name}
               </h1>
             ) : null}
 
             <dl className="grid grid-cols-2 gap-1 text-sm font-medium sm:gap-2 lg:text-base">
-              <dd className="font-semibold">Date</dd>
+              <dd className="font-semibold">When</dd>
               <div>
                 {eventDate && <dt>{eventDate}</dt>}
                 {eventTime && <dt>{eventTime}</dt>}
@@ -78,7 +80,7 @@ export default async function EventPage({
             {venue?.name ? (
               <dl className="grid grid-cols-2 gap-1 text-sm font-medium sm:gap-2 lg:text-base">
                 <div className="flex items-start">
-                  <dd className="font-semibold">Venue</dd>
+                  <dd className="font-semibold">Where</dd>
                 </div>
                 <div className="grid gap-1">
                   <dt>{venue.name}</dt>
